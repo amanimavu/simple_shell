fe4be87a17c904b700090458e9b7031eb5ca4ccb
Amani <amanimavu@gmail.com>
2022-11-20 08:43:23 -0800

    trying to free all memory

--- main.h
+++ main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 #include <stddef.h>
-void execute(char *user_input, char *env[], char **av);
+int execute(char **user_input, char *env[], char **av);
 size_t get_arg_count(char *);
 char *get_user_input(void);
 void looping_prompt(char *env[], char **av);
